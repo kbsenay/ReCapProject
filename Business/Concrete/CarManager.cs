b010e0ca79db4b1439df728de7df9fbdd7f2d53e
Kadir <kbsenay@hotmail.com>
2021-02-08 05:03:19 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+        public List<Car> GetByDailyPrice(decimal min, decimal max)
+        {
+            return _carDal.GetAll(p => p.DailyPrice >= min && p.DailyPrice <= max);
+        }
+
+        public List<Car> GetById(int id)
+        {
+            return _carDal.GetAll(p=>p.CarId==id);
+        }
+    }
+}
